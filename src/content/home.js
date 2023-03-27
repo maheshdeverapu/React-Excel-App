b329715ba276302e33a7b075c639d577eb9d0589
Maheshdeverapu <devarapumahesh9@gmail.com>
2023-03-27 15:10:42 +0530

    added react app

--- src/content/home.js
+++ src/content/home.js
@@ -1,91 +1,3 @@
-// import { useState } from "react";
-// import { OutTable, ExcelRenderer } from 'react-excel-renderer';
-// import { PieChart, Pie, Tooltip, CartesianGrid, XAxis, YAxis, Legend, Bar,BarChart } from 'recharts';
-
-// const Home = () => {
-//     const [state, setState] = useState("")
-//     const [stateg, setStateg] = useState("")
-//     const [data, setData] = useState(false)
-//     const fileHandler = (event) => {
-//         let fileObj = event.target.files[0];
-//         //just pass the fileObj as parameter
-//         ExcelRenderer(fileObj, (err, resp) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             else {
-//                 let x = resp.rows
-//                 setStateg(x)
-//                 setState({
-//                     cols: resp.cols,
-//                     rows: resp.rows
-//                 });
-//                 setData(true)
-//             }
-//         });
-//     }
-//     if (data) {
-
-//         console.log(stateg)
-//     }
-//     let xy = [];
-//     for (let i = 1; i < stateg.length; i++) {
-//         xy.push({ name: stateg[i][0], value: stateg[i][2] })
-//     }
-//     console.log(xy)
-
-//     const datas = xy
-//     return (
-//         <div>
-//             <h2>Add Excel file </h2>
-//             <input type="file" onChange={(event) => { fileHandler(event) }} style={{ "padding": "10px" }} />
-//             {!data ? "" :
-//                 <OutTable data={state.rows} columns={state.cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
-//             }
-
-
-//             <h2>Employee Salary</h2>
-//             <PieChart width={400} height={400}>
-//                 <Pie
-//                     dataKey="value"
-//                     isAnimationActive={false}
-//                     data={datas}
-//                     cx="50%"
-//                     cy="50%"
-//                     outerRadius={80}
-//                     fill="#8884d8"
-//                     label
-//                 />
-//                 <Tooltip />
-//             </PieChart>
-            
-//             <BarChart
-//                 width={500}
-//                 height={300}
-//                 data={datas}
-//                 margin={{
-//                     top: 5,
-//                     right: 30,
-//                     left: 20,
-//                     bottom: 5,
-//                 }}
-//             >
-//                 <CartesianGrid strokeDasharray="3 3" />
-//                 <XAxis dataKey="name" />
-//                 <YAxis />
-//                 <Tooltip />
-//                 <Legend />
-//                 <Bar dataKey="value" fill="#8884d8" />
-//                 <Bar dataKey="uv" fill="#82ca9d" />
-//             </BarChart>
-//                 <p>**hover on the chart to get employee Salary</p>
-//         </div>
-
-//     )
-// }
-// export default Home;
-
-
 import { useState } from "react";
 import { OutTable, ExcelRenderer } from 'react-excel-renderer';
 import { PieChart, Pie, Tooltip, CartesianGrid, XAxis, YAxis, Legend, Bar,BarChart,Cell,AreaChart,LineChart } from 'recharts';
@@ -251,7 +163,7 @@ const Home = () => {
 
 {inputData?
 <div>
-    <h2>Bar Chart</h2>
+    <h2>sold in currency</h2>
 <BarChart width={900} height={500} data={bar_chart}>
   <CartesianGrid strokeDasharray="3 3" />
   <XAxis dataKey="brand" />
@@ -265,7 +177,7 @@ const Home = () => {
 :""}
 {inputData?
 <div>
-    <h2>Pie Chart</h2>
+    <h2>total amount</h2>
 <PieChart width={900} height={500}>
   <Pie data={pie_chart} dataKey="brand" nameKey="amount" cx="50%" cy="50%" outerRadius={90} fill="#8884d8" />
   <Pie data={pie_chart} dataKey="amount" nameKey="brand" cx="50%" cy="50%" innerRadius={100} outerRadius={200} fill="#82ca9d" label />
@@ -282,7 +194,7 @@ const Home = () => {
 :""}
 {inputData?
 <div className="line_chart" > 
-    <h2>Line Chart</h2>
+    <h2>customers based on gender</h2>
 <LineChart width={900} height={500} data={line_chart}
   margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
   <XAxis dataKey="brand" />
@@ -290,8 +202,8 @@ const Home = () => {
   <CartesianGrid strokeDasharray="3 3" />
   <Tooltip />
   <Legend verticalAlign="top" height={36}/>
-  <Line name="pv of pages" type="monotone" dataKey="male" stroke="#8884d8" />
-  <Line name="uv of pages" type="monotone" dataKey="female" stroke="#82ca9d" />
+  <Line name="male customers" type="monotone" dataKey="male" stroke="#8884d8" />
+  <Line name="female customers" type="monotone" dataKey="female" stroke="#82ca9d" />
 </LineChart>
 </div>
 
